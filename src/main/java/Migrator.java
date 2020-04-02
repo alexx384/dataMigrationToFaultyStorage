0452c3ccbc97f3f75ede628ae27f4093dad1fc15
Alexey Palenyy <alexx384@yandex.ru>
2020-04-02 21:34:42 +0300

    implement possibility to transfer files from one server to another
    find bug with bad connection. Need to buffer files from old server.
    I propose to read server output by 4kb chunks. If size lower then
    store file in memory otherwise on disk.

--- src/main/java/Migrator.java
+++ src/main/java/Migrator.java
@@ -3,18 +3,36 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
 import java.util.List;
+import java.util.Objects;
 
 public class Migrator {
     private static final String PATH_TO_FILES = "/files";
     private static final String PATH_TO_FILE = "/files/";
+    private static final int DEFAULT_CONNECT_ATTEMPTS = 5;
+    private final String currentStorageUrl;
 
     public Migrator(String currentStorageUrl) {
         this.currentStorageUrl = currentStorageUrl;
     }
-    private final String currentStorageUrl;
 
-    private static List<String> getFilesList(String storageUrl) {
-        return RequestMaster.sendGetRequest(storageUrl + PATH_TO_FILES, 5, inputStream -> {
+    public boolean migrateTo(String newStorageUrl) {
+        List<String> fileList = getFileList(currentStorageUrl);
+        if (fileList == null) {
+            return false;
+        }
+
+        boolean result = true;
+        for (String fileName : fileList) {
+            result &= copyFile(currentStorageUrl, newStorageUrl, fileName);
+        }
+
+        return result;
+    }
+
+    private static List<String> getFileList(String storageServerUrl) {
+        storageServerUrl += PATH_TO_FILES;
+        return RequestMaster.sendGetRequest(storageServerUrl, DEFAULT_CONNECT_ATTEMPTS, null,
+                (inputStream, none) -> {
             List<String> filesList;
             try {
                 ObjectMapper objectMapper = new ObjectMapper();
@@ -26,16 +44,41 @@ public class Migrator {
         });
     }
 
-    public void migrateToNew(String newStorageUrl) {
+    private static boolean copyFile(String fromUrlAddress, String toUrlAddress, String fileName) {
+        fromUrlAddress += PATH_TO_FILE + fileName;
+        String[] toData = {toUrlAddress, fileName};
+        Boolean result = RequestMaster.sendGetRequest(fromUrlAddress, DEFAULT_CONNECT_ATTEMPTS, toData,
+                (inputStream, data) -> sendFileTo(data[0], data[1], inputStream));
+        return Objects.requireNonNullElse(result, false);
+    }
 
+    public static Boolean sendFileTo(String storageServerUrl, String fileName, InputStream fileData) {
+        storageServerUrl += PATH_TO_FILES;
+        return RequestMaster.sendPostRequest(storageServerUrl, DEFAULT_CONNECT_ATTEMPTS, fileName, fileData);
+    }
+
+    public static boolean deleteFile(String storageServerUrl, String fileName) {
+        storageServerUrl += PATH_TO_FILE + fileName;
+        return RequestMaster.sendDeleteRequest(storageServerUrl, DEFAULT_CONNECT_ATTEMPTS);
     }
 
     public static void main(String[] args) {
-        List<String> filesList = Migrator.getFilesList("http://127.0.0.1:8080/oldStorage");
-        if (filesList == null) {
-            System.out.println("Files list is null");
-        } else {
-            System.out.println(filesList.get(0));
-        }
+//        List<String> filesList = Migrator.getFilesList("http://127.0.0.1:8080/oldStorage");
+//        if (filesList == null) {
+//            System.out.println("Files list is null");
+//        } else {
+//            System.out.println(filesList.get(0));
+//        }
+//        InputStream stream = new ByteArrayInputStream("Test1".getBytes(StandardCharsets.UTF_8));
+//        boolean result = Migrator.sendFileTo("http://127.0.0.1:8080/newStorage", "test2", stream);
+//        boolean result = Migrator.deleteFile("http://127.0.0.1:8080/newStorage", "test2");
+//        System.out.println(result);
+//        System.out.println(Migrator.copyFile(
+//                "http://127.0.0.1:8080/oldStorage",
+//                "http://127.0.0.1:8080/newStorage",
+//                "953.txt"
+//        ));
+        Migrator migrator = new Migrator("http://127.0.0.1:8080/oldStorage");
+        System.out.println(migrator.migrateTo("http://127.0.0.1:8080/newStorage"));
     }
 }
--- src/main/java/RequestMaster.java
+++ src/main/java/RequestMaster.java
@@ -1,36 +1,44 @@
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 public class RequestMaster {
     private static final int STATUS_OK = 200;
+    private static final ContentType DEFAULT_CONTENT_TYPE = ContentType.create("text/plain");
+    private static final Header ACCEPT_ALL_HEADER = new BasicHeader("accept", "*/*");
 
-    public static List<String> sendGetRequest(String urlAddress, int attemptsToConnect,
-                                              Function<InputStream, List<String>> mapper) {
-        List<String> result = null;
+    interface Function2In1Out<FI, SI, R> {
+        R apply(FI firstInput, SI secondInput);
+    }
+
+    public static <I, R> R sendGetRequest(String urlAddress, int attemptsToConnect, I passedData,
+                                          Function2In1Out<InputStream, I, R> mapper) {
+        R result = null;
         try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
             HttpGet httpGet = new HttpGet(urlAddress);
 
             for (int i = 0; i < attemptsToConnect && result == null; i++) {
                 try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
-                    System.out.println("----------------------------------------");
                     StatusLine statusLine = response.getStatusLine();
-                    System.out.println(statusLine);
-
                     // Get hold of the response entity
                     HttpEntity entity = response.getEntity();
                     if (entity != null && statusLine.getStatusCode() == STATUS_OK) {
                         try (InputStream inStream = entity.getContent()) {
-                            result = mapper.apply(inStream);
+                            if (inStream != null) {
+                                result = mapper.apply(inStream, passedData);
+                            }
                         } catch (IOException ex) {
                             result = null;
                         }
@@ -41,5 +49,58 @@ public class RequestMaster {
             result = null;
         }
         return result;
+    }
+
+    public static boolean sendPostRequest(String urlAddress, int attemptsToConnect,
+                                          String fileName, InputStream inputFileStream) {
+        boolean isCompleted = false;
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost(urlAddress);
+            httpPost.addHeader(ACCEPT_ALL_HEADER);
+
+            HttpEntity entity = MultipartEntityBuilder
+                    .create()
+                    .addBinaryBody("file", inputFileStream, DEFAULT_CONTENT_TYPE, fileName)
+                    .build();
+            httpPost.setEntity(entity);
+
+            for (int i = 0; i < attemptsToConnect && !isCompleted; i++) {
+                try (CloseableHttpResponse response = httpclient.execute(httpPost)) {
+                    StatusLine statusLine = response.getStatusLine();
+                    String responseMessage = new String(response.getEntity().getContent().readAllBytes());
+                    System.out.println(responseMessage);
+                    if (responseMessage.length() > 10) {
+                        System.out.println("Find");
+                    }
+                    // Get hold of the response entity
+                    if (statusLine.getStatusCode() == STATUS_OK) {
+                        isCompleted = true;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            isCompleted = false;
+        }
+        return isCompleted;
+    }
+
+    public static boolean sendDeleteRequest(String urlAddress, int attemptsToConnect) {
+        boolean isCompleted = false;
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            HttpDelete httpDelete = new HttpDelete(urlAddress);
+
+            for (int i = 0; i < attemptsToConnect && !isCompleted; i++) {
+                try (CloseableHttpResponse response = httpclient.execute(httpDelete)) {
+                    StatusLine statusLine = response.getStatusLine();
+                    // Get hold of the response entity
+                    if (statusLine.getStatusCode() == STATUS_OK) {
+                        isCompleted = true;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            isCompleted = false;
+        }
+        return isCompleted;
     }
 }
